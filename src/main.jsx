5e684832f7ef34296ddd9432746a90fd5084effe
creuwwi <wim.creuwels@faros.be>
2024-05-16 15:08:30 +0200

    Default enable all toggles

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import FlagProvider from "@unleash/proxy-client-react";
+import FeatureFlags from "./FeatureFlags.js";
 
 const config = {
     url: 'http://localhost:4242/api/frontend', // Your front-end API URL or the Unleash proxy's URL (https://<proxy-url>/proxy)
     clientKey: 'default:development.6affb057bf9b5e190eebc2c65b51333b5ef6354d21af0300f52c5483', // A client-side API token OR one of your proxy's designated client keys (previously known as proxy secrets)
     appName: 'unleash_flags_frontend', // The name of your application. It's only used for identifying your application
     refreshInterval: 60, // How often (in seconds) the client should poll the proxy for updates
-    disableMetrics: true, bootstrap: [{name: 'release_toggle', enabled: false}]
+    disableMetrics: true,
+    bootstrap: [{name: FeatureFlags.RELEASE_TOGGLE, enabled: true},
+        {name: FeatureFlags.UPDATE_FLAG, enabled: true},
+        {name: FeatureFlags.NASA, enabled: true},
+        {name: FeatureFlags.SAVE_FLAG, enabled: true},
+        {name: FeatureFlags.COLOUR_PICKER, enabled: true},
+        {name: FeatureFlags.CHARGE, enabled: true},
+        {name: FeatureFlags.LIKE_FLAG, enabled: true}]
 };
 
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode>
